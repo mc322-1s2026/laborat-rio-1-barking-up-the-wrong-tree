ea8ad992618d53de265c5543305fc1d3ca8f87ad
eduardokobata <193913496+eduardokobata@users.noreply.github.com>
2026-03-20 15:54:02 +0000

    feat: criação dos getters na classe Project; criação do molde das funções com stream em Workspace

--- src/main/java/com/nexus/model/Project.java
+++ src/main/java/com/nexus/model/Project.java
@@ -3,6 +3,7 @@ package com.nexus.model;
 import java.util.ArrayList;
 import java.util.List;
 import com.nexus.model.Task;
+import com.nexus.exception.*;
 
 public class Project {
 
@@ -17,21 +18,44 @@ public class Project {
         this.Nome = nome;
         this.maxWorkload = maxWorkload;
         this.currentWorkload = 0;
-        return;
         //TODO: Implementar verificacoes e validacoes na criacao de projeto!
     }
 
 
     public void addTask(Task task){
-        if (currentWorkload + task.getEffort() > maxWorkload){
-            throw new NexusValidationException("Ultrapassou o limite de workload")
+        if (getCurrentWorkload() + task.getEffort() > maxWorkload){
+            throw new NexusValidationException("Ultrapassou o limite de workload");
         }
-        else{
-            this.Tarefas.add(task);
-            currentWorkload += task.getEffort();
-        }
-        return;
+
+        this.Tarefas.add(task);
+        currentWorkload += task.getEffort();
+        
     }
+
+
+    //Getters
+
+    public List<Task> getTarefas(){
+        return this.Tarefas;
+    }
+
+    public String getNome(){
+        return this.Nome;
+    }
+
+    public int getMaxWorkload(){
+        return this.maxWorkload;
+    }
+
+    public int getCurrentWorkload(){
+
+        return this.getCurrentWorkload();
+    }
+
+    public float getTotalBudget(){
+        return this.totalBudget;
+    }
+
 
 
     
